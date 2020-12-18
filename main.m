67473cfcd2ca540fef54367d3daa1cdd8084bb5e
Aamir Faaiz <aamirfaaiz@Aamirs-MacBook-Pro.local>
2020-12-18 19:14:50 +0530

    change

--- main.m
+++ main.m
@@ -1,5 +1,5 @@
 clc;
 clf;
 close all;
-image = im2double(imread('training_images/train08.jpg'));
+image = im2double(imread('training_images/train01.jpg'));
 [numA,numB] = count_lego(image,true) %Plotting Figures
